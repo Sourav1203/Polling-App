95874b3d7357c2c68752492370f2f3451c1557cc
Sourav1203 <167653067+Sourav1203@users.noreply.github.com>
2025-04-09 17:50:11 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBFHPHbDbQReF1aPSmuX6Xl11nhlCaE6K4",
-    authDomain: "polling-app-5dc55.firebaseapp.com",
-    projectId: "polling-app-5dc55",
-    storageBucket: "polling-app-5dc55.firebasestorage.app",
-    messagingSenderId: "33899263991",
-    appId: "1:33899263991:web:4a4facc024fe4bbadbe0f2",
-    measurementId: "G-NY3J99LHXB"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 // Initialize Firebase
